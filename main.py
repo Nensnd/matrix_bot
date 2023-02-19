85bb00960b95a6fec9d5a7f769a65bd697e51c8a
Nensnd <70484039+Nensnd@users.noreply.github.com>
2023-02-19 17:32:29 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 import telebot
 from telebot import types
 import time
-token = '' #API токен бота
+token = '6233306362:AAGa3XQ10aESz5vYoJV9lR-r22qBX6AQd9g' #API токен бота
 
 bot = telebot.TeleBot(token)
 
@@ -16,13 +16,70 @@ def main(message):
     bot.send_message(message.chat.id, text='Чтобы поменять состояние пикселя , нужно написать команду в таком формате:')
     bot.send_message(message.chat.id, text='состояние пикселя (0 - черный, 1 - белый, 2 - красный, 3 - оранжевый, 4 - желтый, 5 - зеленый, 6 - голубой, 7 - фиолетовый, 8 - серый), координата по x (0 - 10240), координата по y (0 - 10240)')
     bot.send_message(message.chat.id, text='пример: "5 160 90"')
+    bot.send_message(message.chat.id, text='Чтобы просто просмотреть маарицу , введите /view и координаты , выведется кусочек матрицы размером 1024x1024')
+    bot.send_message(message.chat.id, text='пример: "/view 160 70"')
 
 @bot.message_handler(commands=['map'])
 def map(message):
     map = Image.open('map.png')
     bot.send_photo(message.chat.id, map)
-#обработка команд
 
+@bot.message_handler(commands=['view'])
+def view(message):
+    m = str(message.text).replace("/view ", "")
+    ax = []
+    ay = []
+    x = ''
+    y = ''
+    c = 0
+    for i in m:
+        if str(i) == ' ':
+            c = c + 1
+        if c == 0:
+            ax.append(str(i))
+        else:
+            ay.append(str(i))
+
+
+    for i in range(len(ax)):
+        x = x + ax[i]
+
+    for i in range(len(ay)):
+        y = y + ay[i]
+
+
+    cx = int(int(x) / 2048)
+    cy = int(int(y) / 2048)
+
+    if cx < 1:
+        cx = 1
+
+    if cy < 1:
+        cy = 1
+
+    img_01 = Image.open("C:/Users/maxga/PycharmProjects/matrix_bot/matrix/"+str(cx)+"/"+str(cy)+".png")
+    img_02 = Image.open("C:/Users/maxga/PycharmProjects/matrix_bot/matrix/"+str(cx + 1)+"/"+str(cy)+".png")
+    img_03 = Image.open("C:/Users/maxga/PycharmProjects/matrix_bot/matrix/"+str(cx)+"/"+str(cy + 1)+".png")
+    img_04 = Image.open("C:/Users/maxga/PycharmProjects/matrix_bot/matrix/"+str(cx + 1)+"/"+str(cy + 1)+".png")
+
+    img_01_size = img_01.size
+    new_im = Image.new('RGB', (2 * img_01_size[0], 2 * img_01_size[1]), (250, 250, 250))
+
+    new_im.paste(img_01, (0, 0))
+    new_im.paste(img_02, (img_01_size[0], 0))
+    new_im.paste(img_03, (0, img_01_size[1]))
+    new_im.paste(img_04, (img_01_size[0], img_01_size[1]))
+    new_im.save("view.png", "PNG")
+    new_im.close()
+    new_im = Image.open('view.png')
+    cx = cx - 1
+    cy = cy - 1
+    croped = new_im.crop(((int(x) - cx * 2048) - 512, (int(y) - cy * 2048) - 512, (int(x) - cx * 2048) + 513,(int(y) - cy * 2048) + 513))
+    croped.save('view.png', 'PNG')
+    c_end = croped.resize((2561, 2561))
+    c_end.save('view.png', 'PNG')
+    bot.send_photo(message.chat.id, c_end)
+#обработка команд
 @bot.message_handler(content_types=['text'])
 def mainloop(message):
     t = 0
@@ -201,36 +258,37 @@ def mainloop(message):
             draw = ImageDraw.Draw(img)
             draw.rectangle((int(x) - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - cx * 2048, int(y) - 2048 - cy * 2048), fill='darkgrey')
 
-        # img_04
-        elif int(x) > 2049 and int(y) > 2048:
-            img = img_04
-            if t == 1:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048), fill='white')
-            elif t == 0:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='black')
-            elif t == 2:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='red')
-            elif t == 3:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='orange')
-            elif t == 4:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='yellow')
-            elif t == 5:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='green')
-            elif t == 6:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='blue')
-            elif t == 7:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='purple')
-            elif t == 8:
-                draw = ImageDraw.Draw(img)
-                draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='darkgrey')
+
+    # img_04
+    elif int(x) > 2048 and int(y) > 2048:
+        img = img_04
+        if t == 1:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048), fill='white')
+        elif t == 0:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='black')
+        elif t == 2:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='red')
+        elif t == 3:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='orange')
+        elif t == 4:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='yellow')
+        elif t == 5:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='green')
+        elif t == 6:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='blue')
+        elif t == 7:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='purple')
+        elif t == 8:
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048, int(x) - 2048 - cx * 2048, int(y) - 2048 - cy * 2048),fill='darkgrey')
 
     cx = int(int(x) / 2048)
     cy = int(int(y) / 2048)
